0e7c142523bcbd8b47a70164b7a944473d2441ff
Radek Holý <Radek1@Holych.org>
2016-07-10 09:37:49 +0200

    Add an ant skin

---
+++ skin.py
@@ -0,0 +1,30 @@
+# Copyright 2016 RunSugarRun authors.
+# See the AUTHORS file found in the top-level directory of this
+# distribution and at https://github.com/kosova-holy/Ant.
+#
+# Licensed under the MIT License. See the LICENSE file found in the
+# top-level directory of this distribution and at
+# https://github.com/kosova-holy/Ant. No part of RunSugarRun, including
+# this file, may be copied, modified, propagated, or distributed except
+# according to the terms contained in the LICENSE file.
+#
+# This document provides a complete list of all the contributors that
+# have ever contributed to RunSugarRun in order to give them credit.
+
+skin = [
+    ('hind_right_femur', (14, 103), 'femur.png', False, False, 1),
+    ('hind_right_tibia', (77, 180), 'tibia_right_hind.png', False, False, 1),
+    ('middle_right_femur', (14, 103), 'femur.png', False, False, 1),
+    ('middle_right_tibia', (77, 180), 'tibia_right_middle.png', False, False, 1),
+    ('front_right_femur', (14, 103), 'femur.png', True, False, 1),
+    ('front_right_tibia', (20, 141), 'tibia_front.png', False, False, 1),
+    ('right_antenna', (133, 154), 'antenna.png', True, True, 1),
+    ('body', (241, 502), 'body.png', True, True, 1),
+    ('left_antenna', (133, 154), 'antenna.png', True, True, 1),
+    ('front_left_femur', (14, 103), 'femur.png', True, False, 1),
+    ('front_left_tibia', (20, 141), 'tibia_front.png', False, False, 1),
+    ('middle_left_femur', (14, 103), 'femur.png', True, False, 1),
+    ('middle_left_tibia', (33, 174), 'tibia_left_middle.png', False, False, 1),
+    ('hind_left_femur', (14, 103), 'femur.png', True, False, 1),
+    ('hind_left_tibia', (33, 174), 'tibia_left_hind.png', False, False, 1),
+  ]
